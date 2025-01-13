d4a4bd4c92bd071769c9b58f554b2e0acb4a754f
Dhanraj Sunil Chitte <156110967+DhanrajChitte@users.noreply.github.com>
2025-01-13 17:55:41 +0530

    Add files via upload

--- MongoSpringBoot/src/main/java/com/MongoSpringBoot/MongoSpringBoot/MongoSpringBootApplication.java
+++ MongoSpringBoot/src/main/java/com/MongoSpringBoot/MongoSpringBoot/MongoSpringBootApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MongoSpringBootApplication {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		SpringApplication.run(MongoSpringBootApplication.class, args);
 	}
-
 }
